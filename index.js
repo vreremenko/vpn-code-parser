a444ce1728ae0e2139d4478cde34f025ca448578
vovan4ik9696 <vovan4ik9696@mail.ru>
2024-12-17 08:54:59 +0300

    add express

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const puppeteer = require("puppeteer");
-const axios = require("axios");
+const express = require("express");
 
 const sendCode = require('./sendCode');
 const {createMailBox, deleteMailBox} = require('./mailBoxCreator');
 
-(async () => {
-    const {id, email} = await createMailBox();
-    await sendCode(email)
-    await deleteMailBox(id);
-})();
+const app = express();
+
+app.get('/', async (req, res) => {
+    try {
+        const {id, email} = await createMailBox();
+        await sendCode(email)
+        await deleteMailBox(id);
+        res.send("Process complete successfully.");
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("An error occurred.");
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 
